e3be5429725d80457e973c4cb89daf930111b66f
wopanda <137699242@qq.com>
2025-09-14 21:35:19 +0800

    fix deploy script encoding issue

--- deploy.bat
+++ deploy.bat
@@ -1,27 +1,12 @@
 @echo off
-chcp 65001 > nul
-setlocal enabledelayedexpansion
-
-rem 读取提交信息：优先级：1) 命令行参数 2) 发布文本.txt 第一行 3) 时间戳
 set "MSG=%~1"
-if "%MSG%"=="" (
-  if exist "发布文本.txt" (
-    for /f "usebackq delims=" %%A in ("发布文本.txt") do (
-      if "!MSG!"=="" if not "%%A"=="" set "MSG=%%A"
-    )
-  )
-)
-if "!MSG!"=="" (
-  for /f "tokens=1-4 delims=/-:. " %%a in ("%date% %time%") do set "TS=%%a-%%b-%%c_%%d"
-  set "MSG=自动发布-!TS!"
-)
+if "%MSG%"=="" set "MSG=update"
 
-echo 本次提交信息: !MSG!
 echo Building site...
 hugo --cleanDestinationDir
 echo Build done.
 
-echo Copying public/* to repo root...
+echo Copying public to repo root...
 xcopy /E /Y /I public\* . > nul
 
 echo Creating .nojekyll...
@@ -29,6 +14,6 @@ echo.> .nojekyll
 
 echo Committing and pushing...
 git add -A
-git commit -m "!MSG!"
+git commit -m "%MSG%"
 git push
 echo Deploy success.
