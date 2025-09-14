23b9e858041a4000c89b8dfebff50eb7f60a9ed8
wopanda <137699242@qq.com>
2025-09-14 21:33:44 +0800

    !MSG!

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,10 @@
 @echo off
 chcp 65001 > nul
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 
-rem 提交信息优先级：1) 命令行参数 2) 发布文本.txt 第一行 3) 时间戳
+rem 读取提交信息：优先级：1) 命令行参数 2) 发布文本.txt 第一行 3) 时间戳
 set "MSG=%~1"
-if "!MSG!"=="" (
+if "%MSG%"=="" (
   if exist "发布文本.txt" (
     for /f "usebackq delims=" %%A in ("发布文本.txt") do (
       if "!MSG!"=="" if not "%%A"=="" set "MSG=%%A"
@@ -15,6 +15,8 @@ if "!MSG!"=="" (
   for /f "tokens=1-4 delims=/-:. " %%a in ("%date% %time%") do set "TS=%%a-%%b-%%c_%%d"
   set "MSG=自动发布-!TS!"
 )
+
+echo 本次提交信息: !MSG!
 echo Building site...
 hugo --cleanDestinationDir
 echo Build done.
