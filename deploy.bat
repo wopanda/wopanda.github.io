448843571bf10190e10c0599d974ea70f3424f1d
wopanda <137699242@qq.com>
2025-09-20 23:17:43 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,10 @@
 set "MSG=%~1"
 if "%MSG%"=="" set "MSG=update"
 
+echo Syncing data folder...
+xcopy /E /Y /I data\* static\data\ > nul
+echo Data sync done.
+
 echo Building site...
 hugo --cleanDestinationDir
 echo Build done.
